190dc0d2a342d53af6e94834bea26e3bb0d6d7ec
SivoP <siv.padhy@gmail.com>
2015-08-09 16:32:43 -0700

    created a checkbox that toggles an event's completion status. Currently implemented on client side only, server side changes to document still in the works

--- client/templates/project/event/event.js
+++ client/templates/project/event/event.js
@@ -1,15 +1,32 @@
 Template.newEvent.helpers({
+  // find all projects
   projects: function(){
     return Projects.find()
   },
+
+  // finds the events of a project (an array)
   events: function(){
     return Projects.findOne().events
   },
 
+   // targets a single event in the array
   singleEvent: function(){
-   var events = Projects.findOne().events
-   console.log(events)
-   return events[0]
-  }
+    var events = Projects.findOne().events
+    return events[0]
+  },
+
+  eventCompleted: function(){
+    if(Session.get("eventCompleted")){
+      return true
+    }
+  },
+
+
 
 });
+
+Template.newEvent.events({
+  "change .event-completed input": function(e){
+    Session.set("eventCompleted", event.target.checked)
+  }
+})
