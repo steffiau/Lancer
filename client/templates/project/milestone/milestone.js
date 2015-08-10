d1970d463d07a51bb80ffdd939af0c1fceb551e8
Steffi Au <steffi.myau@gmail.com>
2015-08-10 12:06:01 -0700

    Added comment textfield and button, work in progress for saving data into database

--- client/templates/project/milestone/milestone.js
+++ client/templates/project/milestone/milestone.js
@@ -1,31 +1,22 @@
 var projectId = "AdkNuqkxZehbGCh2b";
 
 Template.singleMilestone.helpers({
-  // find the project
-  project: function(){
-    return Projects.findOne({ _id : projectId })
-  },
-
-  // finds the events of the project (an array)
-  milestones: function(){
-    return Projects.findOne({ _id : projectId }).milestones
-  },
-
    // targets a single event in the array
   singleMilestone: function(){
     return Projects.findOne({ _id : projectId }).milestones[0]
   },
-
-  milestoneCompleted: function(){
-    if(Session.get("milestoneCompleted")){
-      return true
-    }
-  },
-
   requirementCompleted: function(){
     if(Session.get("milestoneCompleted")){
       return true
     }
   },
+  milestoneCompleted: function(){
+    if(Session.get("milestoneCompleted")){
+      return true
+    }
+  },  
+});
 
+Template.singleMilestone.events({
+  ""
 });
