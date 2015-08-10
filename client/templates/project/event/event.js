4cc627df14ac5d6ac6311c16f7a0f853cf16e0c3
SivoP <siv.padhy@gmail.com>
2015-08-09 21:46:20 -0700

    commiting to acquire project data structure changes

--- client/templates/project/event/event.js
+++ client/templates/project/event/event.js
@@ -24,27 +24,29 @@ Template.singleEvent.helpers({
 
 Template.singleEvent.events({
   "change .event-completed input": function(e){
+    e.stopPropagation
     var project = Projects.findOne()
     var projectId = project._id
     var events = Projects.findOne().events
     var currentEvent = events[0]
     Session.set("eventCompleted", event.target.checked)
-    Projects.update({_id: projectId}, {$set:  {'events.0.completed': !currentEvent.completed}})
-    
+    var completeStatus = Session.get("eventCompleted")
+    Projects.update({_id: projectId}, {$set:  {'events.0.completed': completeStatus }})
+    console.log(events[0].completed)
+   
   },
   "blur .single-event-details li": function(e){
     var currentProject = Projects.findOne()._id
     var title = document.getElementById("single-event-title").innerHTML
     var location = document.getElementById("single-event-location").innerHTML
     var date = document.getElementById("single-event-date").innerHTML
-    var  notes = document.getElementById("single-event-notes").innerHTML
+    var notes = document.getElementById("single-event-notes").innerHTML
     var eventAttributes = {
       title: title,
       location: location,
       date: date,
       notes: notes 
     }   
-     console.log(this)
   }
 })
 // helpers required for all changes to events page being reflected in the database
