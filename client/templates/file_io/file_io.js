1f8d7ece6b69e6da83abfcae4b20aea155474d4f
SivoP <siv.padhy@gmail.com>
2015-08-11 14:12:57 -0700

    uploaded file is properly recognized in the html template, have not implemented server side logic yet

--- client/templates/file_io/file_io.js
+++ client/templates/file_io/file_io.js
@@ -1,8 +1,25 @@
-// defines an FS collection called projectfiles
+// defines an FS File STORE called projectFilesStore
 // will come up in mongoDB as cfs.projectFiles.filerecord
-var projectFiles = new FS.Collection("projectfiles",{
+var projectFilesStore = new FS.Collection("projectfiles",{
   stores: [new FS.Store.FileSystem("images", {path: "Lancer/lib/projectfiles"})]
 });
 
+// defines a new FS COLLECTION named called projectFiles
+var projectFiles = new FS.Collection('projectFiles', {
+  stores: [projectFilesStore]
+});
 
 
+// template helpers for the upload form
+// helper is specific to client as collectionFS suggests functionality is problematic when running on server side
+
+// if(Meteor.isClient){
+  Template.uploadForm.events({
+    "click h1": function(e){
+      var file = $("#file").get(0).files[0]
+     
+     
+    }
+  });
+
+// }
