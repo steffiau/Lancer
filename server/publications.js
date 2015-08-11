1233172b03eaa826b37938ea222996216ae0c592
Tom Lei - Mac <lmd0209@msn.com>
2015-08-11 16:59:15 -0700

    Take off autopublish Success ^-^#

--- server/publications.js
+++ server/publications.js
@@ -1,1 +1,12 @@
 //  Meteor.publish definitions
+Meteor.publish('projects', function() {
+	var id = this.userId;
+	return Projects.find({owner_id:id});
+});
+Meteor.publish('clients', function() {
+	var id = this.userId;
+	return Clients.find({owner_id:id});
+});
+
+
+
