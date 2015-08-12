57340bca2cdcf4fdf0f72b25498d3b146bf877f4
SivoP <siv.padhy@gmail.com>
2015-08-11 21:51:07 -0700

    file upload saves file to FS collection and fileObject to projects collection, still needs to be saved in the right event(milestone)

--- client/templates/file_io/file_io.js
+++ client/templates/file_io/file_io.js
@@ -1,8 +1,7 @@
 // defines an FS File STORE called projectFilesStore
 // will come up in mongoDB as cfs.projectFiles.filerecord
-var projectFilesStore = new FS.Collection("projectFiles",{
-  path: "Lancer/lib/projectfiles"})
-});
+var projectFilesStore = new FS.Store.FileSystem("projectFiles",{
+  path: "Lancer/lib/projectfiles"});
 
 // defines a new FS COLLECTION named called projectFiles
 var projectFiles = new FS.Collection('projectFiles', {
@@ -10,14 +9,46 @@ var projectFiles = new FS.Collection('projectFiles', {
 });
 
 
-// template helpers for the upload form
+projectFiles.allow({
+  insert: function () {
+    return true;
+  },
+  update: function () {
+    return true;
+  },
+  remove: function () {
+    return true;
+  },
+  download: function () {
+    return true;
+  }
+});
 
-  Template.uploadForm.events({
-    "submit form": function(e){
-      debugger
-      var file = $("#file").get(0).files[0]
-      var fileObj = projectFiles.insert(file)
-      console.log(e)
-    }
-  });
+
+// Template.uploadForm.helpers({
+//   var project = Projects.findOne({_id: Session.get("projectId")});
+//     return project.events[Session.get("event_index")]
+//   }
+// })
+
+
+// template helpers for the upload form
+Template.uploadForm.events({
+  "click h1": function(e){
+    var project =  Projects.findOne();
+    var file = $("#file").get(0).files[0];
+    var fileObj = projectFiles.insert(file);
+    var currentEvent = project.events[Session.get("event_index")]
+   
+    console.log(currentEvent)
+    // Projects.update(
+    //   {_id: project},
+    //   {$addToSet: {*target: *file}  }
+    // )
+  }
+});
+
+
+
+
 
