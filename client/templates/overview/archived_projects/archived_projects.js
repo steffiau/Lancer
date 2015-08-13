2ddd5e86ab1e16fa87765ce033402c044e9f256d
Tom Lei - Mac <lmd0209@msn.com>
2015-08-12 17:07:23 -0700

    implemented archieved_project table with data-table

--- client/templates/overview/archived_projects/archived_projects.js
+++ client/templates/overview/archived_projects/archived_projects.js
@@ -1,6 +1,15 @@
 Template.archivedProjects.helpers({
 	archived_projects: function(){
 		return Projects.find({project_finished:true});
+	},
+	settings: function(){
+	return {fields:[{key: "name",label: "Project Name"},
+	{key: "description", label: "Description"},
+	{key: "start_date", label: "Started at"},
+	{key: "project_finishedAt", label: "Finished at"},
+	{key: "client_id", label: "client_id", fn: function(value,object},
+	{key: "contract_amount", label: "$$$"}]
+	};
 	}
 
 });
