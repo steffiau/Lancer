379e762d5c47a646d68d62a898c07b035190a074
SivoP <siv.padhy@gmail.com>
2015-08-13 13:18:02 -0700

    event completion now matches the correct project, matched by project ID

--- client/templates/project/meeting/meeting.js
+++ client/templates/project/meeting/meeting.js
@@ -8,30 +8,35 @@ Template.meeting.helpers({
 
 Template.meeting.events({
   "click .event-completed button": function(e){
-    var project = Projects.findOne()
-    var projectId = project._id
-    var events = project.events
+    var project = Projects.findOne();
+    var projectId = Session.get("projectId")
+    var events = project.events;
+    var object = {};
+    var index = Session.get("event_index");
+    var completedMod = 'events.' + index + '.completed';
+
     if(events[Session.get("event_index")].completed){
-      Projects.update({_id: projectId}, {$set:  {'events.0.completed': false }})
+      object[completedMod] = false
+      Projects.update({_id: projectId}, {$set: object})
     } else {
-       Projects.update({_id: projectId}, {$set:  {'events.0.completed': true }})
+      object[completedMod] = true
+       Projects.update({_id: projectId}, {$set:  object})
     }
     //   Session.set("eventCompleted", !event.target.checked)
-    var project = Projects.findOne()
-    var projectId = project._id
-    var events = project.events
-   
+    console.log(object)
+    console.log(this)
+    console.log(Session.get("event_index"))   
   },
   "blur .single-event-details li": function(e){
 
-    var currentProject = Projects.findOne()._id
+    var currentProject = Projects.findOne()._id;
     // variables below grab changes to the event-details by the user
     // these are meant to be sent to mongo to update on server-side
 
-    var title = document.getElementById("single-event-title").innerText
-    var location = document.getElementById("single-event-location").innerText
-    var date = document.getElementById("single-event-date").innerText
-    var notes = document.getElementById("single-event-notes").innerText
+    var title = document.getElementById("single-event-title").innerText;
+    var location = document.getElementById("single-event-location").innerText;
+    var date = document.getElementById("single-event-date").innerText;
+    var notes = document.getElementById("single-event-notes").innerText;
 		var index = Session.get("event_index");
 		var obj = {};
 		var titleMod = 'events.' + index + '.title';
