8ee224af4bd6019d915a4edf59fe20c68f760dbc
Steffi Au <steffi.myau@gmail.com>
2015-08-13 18:34:24 -0700

    Publishing projects that user has a collaboration role, by adding their collcollab_id to the project document

--- server/publications.js
+++ server/publications.js
@@ -2,12 +2,12 @@
 Meteor.publish('projects', function() {
 	var id = this.userId;
   // return Projects.find({});
-  return Projects.find({owner_id:id});
+  return Projects.find({ $or: [{owner_id:id}, {collab_id:id}] });
 });
 Meteor.publish('clients', function() {
 	var id = this.userId;
   // return Clients.find({});
-	return Clients.find({owner_id:id});
+	return Clients.find({ $or: [{owner_id:id}, {collab_id:id}] });
 });
 
 Meteor.publish('projectFiles', function() {
