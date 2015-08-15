93192168ce83612ace84920abc3fd2972eb3f4d2
Tom Lei - Mac <lmd0209@msn.com>
2015-08-15 14:54:53 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,6 +3,10 @@
 git checkout development
 git pull origin development
 git merge $1
+read -r -p "Keep going? [Y/n]" response
+response=${response,,} # tolower
+if [[ $response =~ ^(yes|y| ) ]]; then
 git push
 git checkout $1
 git pull origin development
+fi
