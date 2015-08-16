79670b96f6ebfc801e62441715692299ff45e5bf
SivoP <siv.padhy@gmail.com>
2015-08-15 19:20:28 -0700

    editting publication permissions

--- client/main.js
+++ client/main.js
@@ -2,4 +2,4 @@ Meteor.subscribe('projects');
 Meteor.subscribe('clients');
 Meteor.subscribe('projectFiles');
 Meteor.subscribe('allUsers');
-Meteor.subscribe('messages');
+Meteor.subscribe('messages', Session.get("projectId"));
