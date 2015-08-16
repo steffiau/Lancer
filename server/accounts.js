7df1d642e638ac22c5523f0a775efe1292ed8737
Tom Lei - Mac <lmd0209@msn.com>
2015-08-15 18:52:22 -0700

    Added new parameter in user profile, login_count

--- server/accounts.js
+++ server/accounts.js
@@ -1,6 +1,7 @@
 Accounts.onCreateUser(function(options,user){
 	if (!user.profile) { user.profile = {} ;};
 	if (options['email']) {
+		// This is normal user 
 		user.profile['email'] = options['email'];
 		return user;
 	}
@@ -9,6 +10,7 @@ Accounts.onCreateUser(function(options,user){
 		user.profile['name'] = user.services.google.name;
 		user.profile['picture'] = user.services.google.picture;
 	}
+		user.profile['login_count'] = 0;
 	return user;
 });
 
