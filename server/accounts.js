befd70d9172bce8a10299c93e4d76dbfaeed3d52
Tom Lei - Mac <lmd0209@msn.com>
2015-08-15 19:18:42 -0700

    user login count implemented

--- server/accounts.js
+++ server/accounts.js
@@ -3,14 +3,20 @@ Accounts.onCreateUser(function(options,user){
 	if (options['email']) {
 		// This is normal user 
 		user.profile['email'] = options['email'];
+		user.profile['login_count'] = 0;
 		return user;
 	}
 	if (user.services.google.email) {
 		user.profile['email'] = user.services.google.email;
 		user.profile['name'] = user.services.google.name;
 		user.profile['picture'] = user.services.google.picture;
-	}
 		user.profile['login_count'] = 0;
+	}
 	return user;
 });
 
+Accounts.onLogin(function(user){
+	Meteor.users.update({'_id': user.user._id},{$inc:{"profile.login_count" : 1}});
+	console.log(user.user.profile['login_count']);
+
+});
