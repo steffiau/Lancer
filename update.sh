bbc34422112c8e1d02efee1ce4a57ea28c2b3143
Tom Lei - Mac <lmd0209@msn.com>
2015-08-15 18:38:34 -0700

    merge

--- update.sh
+++ update.sh
@@ -4,10 +4,6 @@ git checkout development
 git pull origin development
 git merge $1
 
-read -n1 -r -p "Press space to continue..." key
-
-if [ "$key" = ' ' ]; then
 git push
 git checkout $1
 git pull origin development
-fi
