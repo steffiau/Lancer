2dfb9e6036abfe6f3d434cca29ab2f081524cbc1
Tom Lei - Mac <lmd0209@msn.com>
2015-08-16 22:14:03 -0700

    storing user's stripe data in profile

--- client/templates/stripe/stripe.js
+++ client/templates/stripe/stripe.js
@@ -3,8 +3,12 @@ Template.getStripeUserSecret.helpers({
 		var code = window.location.search.split('code=')[1];
 		console.log(code);
 		Meteor.call("getStripeSecret",code,function(error,result){
-			console.log(error);
-		console.log(result);
+			if (!error){
+				var stripe = _.pick(JSON.parse(result.content),"access_token","refresh_token","stripe_publishable_key","stripe_user_id");
+				Meteor.users.update({"_id":currentUser()._id},{$set: {"profile.stripe": stripe }});
+				} else {
+				alert("Failed");
+				};
 		});
 	}
 	
