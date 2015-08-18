389028a5756e99f37fce0cf819292aaaee8f3753
Tom Lei - Mac <lmd0209@msn.com>
2015-08-17 18:23:45 -0700

    added invoice route and ensured the for works

---
+++ client/templates/project/invoice_route/invoice.js
@@ -0,0 +1,47 @@
+Template.invoiceRoute.helpers ({
+	returndata: function(){
+		console.log(this);
+		return this;
+	},
+  user_email: function () {
+    return this.user.profile.email;
+  },
+  project: function () {
+    return this.project;
+  },
+  invoice: function () {
+    return this.event;
+  },
+	user: function(){
+		return this.user;
+	},
+  invoiceitems: function () {
+    return  _.map(this.event.items, function(value, index){
+      return {value: value, index: index};
+    });
+  },
+  client: function () {
+    return this.client;
+  },
+  subtotal: function () {
+    var subtotal = _.reduce(this.event.items, function(sum, num) {
+      return sum + (num.qty * num.price);
+    }, 0);
+    return "$" + subtotal.toFixed(2);
+  },
+  tax: function () {
+    var subtotal = _.reduce(this.event.items, function(sum, num) {
+      return sum + (num.qty * num.price);
+    }, 0);
+    return "$" + (subtotal * 0.12).toFixed(2);
+  },
+  grandtotal: function () {
+    var subtotal = _.reduce(this.event.items, function(sum, num) {
+      return sum + (num.qty * num.price);
+    }, 0);
+    return "$" + (subtotal * 1.12).toFixed(2);
+  }
+});
+
+Template.invoiceRoute.events({
+ });
