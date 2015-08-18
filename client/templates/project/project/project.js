c3511116aa99c1460d1379c3accb79e4759f0485
Steffi Au <steffi.myau@gmail.com>
2015-08-17 17:39:33 -0700

    Added delete collab user function

--- client/templates/project/project/project.js
+++ client/templates/project/project/project.js
@@ -21,5 +21,19 @@ Template.project.events({
     var userEmail = e.currentTarget[0].value;
     var newUserId = Meteor.users.findOne({"profile.email":userEmail})._id;
     Projects.update({_id: Session.get("projectId")}, {$addToSet: {"collabId":newUserId}});
-  }
+  },
+  "click .delete-collab": function(e) {
+    var projectId = Session.get("projectId");
+    var collabId = e.currentTarget.dataset.id;
+
+    // var objRemove = {};
+    // var reqItem = "events." + index + ".requirements." + reqIndex;
+    // objRemove[reqItem] = 1;
+    // Projects.update({_id: projectId}, {$unset: objRemove})
+
+    var objClear = {};
+    // var reqArray = "events." + index + ".requirements";
+    objClear["collabId"] = collabId;
+    Projects.update({_id: projectId}, {$pull: objClear})
+  },
 });
