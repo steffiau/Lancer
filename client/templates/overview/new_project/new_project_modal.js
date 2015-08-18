fe05a44d05cc8bafd75e2bc6fabc97eb495ed8cd
Wes <wesley.ch.yu@gmail.com>
2015-08-17 23:00:12 -0700

    WIP

--- client/templates/overview/new_project/new_project_modal.js
+++ client/templates/overview/new_project/new_project_modal.js
@@ -1,10 +1,4 @@
 
-Template.newProjectModal.helpers({
-  selected: function(e){
-    console.log(e);
-    return true
-  }
-})
 
 Template.newProjectModal.onRendered( function() {
   this.$('#start-date-picker').datetimepicker({
@@ -19,13 +13,14 @@ Template.newProjectModal.events({
   "click .next-button": function(e) {
     var next_element =  $(e.currentTarget).parent().next();
     var move = next_element.parent().scrollTop() + next_element.position().top
-    console.log(move)
     next_element.addClass("selected-question").children()[1].focus();
     next_element.siblings().removeClass("selected-question");
-    console.log(next_element);
     $(".modal-content").animate({
       scrollTop: move
     }, 500);
+  },
+  "keyup .modal-short-text-response": function(e) {
+    console.log(e)
   }
 })
 
